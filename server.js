6fb1daab33528d37f23c312bc569f9ca29e93aa8
Amala <amalajoshy@gmail.com>
2016-01-16 14:21:40 -0800

    Changed server.js to print hello world message

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+app.get('/hello', function(req, res){
+  res.send('hello world');
+});
+
 app.listen(port, ipaddress);
